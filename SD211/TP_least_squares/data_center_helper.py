0b722c17ecd880be17f7ac8b190295f29eb07209
lup1n <780966523@qq.com>
2024-01-18 23:52:16 +0100

    submitted to ecampus

--- SD211/TP_least_squares/data_center_helper.py
+++ SD211/TP_least_squares/data_center_helper.py
@@ -22,18 +22,18 @@ A_test = np.hstack([M_test, np.ones((M_test.shape[0],1)), -(M_test.T * COP_test[
 b_test = COP_test[:,3]
 
 
-# Loading raw data
-import pandas as pd
-data = pd.read_csv('Raw_Dataset_May.csv')
+# # Loading raw data
+# import pandas as pd
+# data = pd.read_csv('Raw_Dataset_May.csv')
 
-def name_to_subcategory_and_details(col_name):
-    if np.isreal(col_name):
-        col_name = names[col_name]
-    indices = np.nonzero((data['NAME'] == col_name).values)[0]
-    if len(indices) > 0:
-        subcategory = data['SUBCATEGORY'].iloc[[indices[0]]].values[0]
-        details = data['DETAILS'].iloc[[indices[0]]].values[0]
-        return subcategory, details
-    else:
-        print('unknown name')
+# def name_to_subcategory_and_details(col_name):
+#     if np.isreal(col_name):
+#         col_name = names[col_name]
+#     indices = np.nonzero((data['NAME'] == col_name).values)[0]
+#     if len(indices) > 0:
+#         subcategory = data['SUBCATEGORY'].iloc[[indices[0]]].values[0]
+#         details = data['DETAILS'].iloc[[indices[0]]].values[0]
+#         return subcategory, details
+#     else:
+#         print('unknown name')
 
